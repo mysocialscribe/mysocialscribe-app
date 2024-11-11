c0c616182946c952bfa3b04a377a777acdd53c17
husamahmud <husamahmud@gmail.com>
2024-11-11 14:18:32 +0300

    feat implement GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-import type { Metadata } from 'next'
 import clx from 'clsx'
+import type { Metadata } from 'next'
+import Script from 'next/script'
 import { Lato } from 'next/font/google'
 
-import { GoogleAnalytics } from '@next/third-parties/google'
 import { ThemeProvider } from '@/app/providers'
 import './globals.css'
 
@@ -22,6 +22,9 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID
+  if (!GTM_ID) console.warn('GTM_ID is not defined')
+
   return (
     <html
       lang="en"
@@ -33,9 +36,32 @@ export default function RootLayout({
           href="/icon.png"
           type="image/png"
         />
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="beforeInteractive"
+        >
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
       </head>
 
       <body className={clx('bg-zinc-50 dark:bg-zinc-950', lato.className)}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
@@ -44,8 +70,6 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
-
-        <GoogleAnalytics gaId="" />
       </body>
     </html>
   )
